ea25c995e6c1de3f71122e2e0a8c2d51952d1f5b
Altant <28458922+Altant457@users.noreply.github.com>
2023-03-06 16:10:09 +0100

    Add animated health bar in player HUD

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    private bool paused = false;
+
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                //Cursor.lockState = CursorLockMode.None;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                //Cursor.lockState = CursorLockMode.Locked;
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+            }
+
+            paused = !paused;
+        }
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+}
